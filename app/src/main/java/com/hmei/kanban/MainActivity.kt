c1b68f18cc618416ad34c8effc2265a10fb414cd
devilinside71 <devilinside71@users.noreply.github.com>
2021-05-09 09:26:58 +0200

    Add FAB

--- app/src/main/java/com/hmei/kanban/MainActivity.kt
+++ app/src/main/java/com/hmei/kanban/MainActivity.kt
@@ -2,10 +2,17 @@ package com.hmei.kanban
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fabAdd = findViewById<FloatingActionButton>(R.id.fabAdd)
+
+        fabAdd.setOnClickListener {
+           Log.e("Add","add item")
+        }
     }
 }
